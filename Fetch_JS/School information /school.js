7c90fe363ba6eab946eb82225c4ae4443e368279
Abhijeet Roy <abhijeetrou123@gmail.com>
2024-05-11 19:05:31 +0530

    Initial commit

--- Fetch_JS/School information /school.js
+++ Fetch_JS/School information /school.js
@@ -33,11 +33,12 @@ const school = {
 };
 
 
-// let sum = 0;
-// for (let key in school.departments) {
-//   sum += school.departments[key].teachers;
-// }
-// console.log(sum);
+let sum = 0;
+for (let key in school.departments) {
+  sum += school.departments[key].teachers;
+}
+console.log(sum);
+// countCalculation
 function countcalulation(school) {
   const {
     departments:{
@@ -63,7 +64,7 @@ function findtopstudent(school ,courseName){
 
 console.log(findtopstudent(school,"history"));
 
-// Adding new department inn the course
+// Adding new department in the course
 
 let newdepartment={art:{teacher:5, students:50}}
 function addnewdpt(school , newdepartment){
@@ -79,6 +80,39 @@ function addnewdpt(school , newdepartment){
 }
 console.log(addnewdpt(school,newdepartment));
 
-function highestStudentCountDepartment(){
-  
+// highestStudentCountDepartment
+function highestStudentCountDepartment(school){
+  const {
+    departments:{
+      math : { students: mathstudentscount },
+      science: { students: sciencestudentscount },
+      history: { students: historystudentscount },
+      english: { students: englishstudentscount },
+    }
+  } = school;
+  const highestcount = Math.max(mathstudentscount,sciencestudentscount,historystudentscount,englishstudentscount);
+  const highestcountdept = highestcount === mathstudentscount? "math" : highestcount === sciencestudentscount? "science" : highestcount === historystudentscount? "history" : highestcount === englishstudentscount? "english" : "";
+  return `highest count dept is ${highestcountdept} with ${highestcount} students`;
 }
+
+console.log(highestStudentCountDepartment(school));
+
+// generates a greeting message for a given name and language
+
+
+function generateGreeting(name, language) {
+  let greeting = "";
+  if (language === "english") {
+    greeting = `Hello, ${name}!`;
+  }
+  if (language === "spanish") {
+    greeting = `Hola, ${name}!`;
+  }
+  if (language === "french") {
+    greeting = `Bonjour, ${name}!`;
+  }
+  return greeting
+}
+console.log(generateGreeting("Alice","english"));
+console.log(generateGreeting("Bob", "spanish"));
+console.log(generateGreeting("Charlie", "french"));
