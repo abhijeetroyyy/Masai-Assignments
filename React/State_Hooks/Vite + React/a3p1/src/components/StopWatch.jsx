ad7a088c1bf505c344d1f6ea19a8c62dca887c24
Abhijeet Roy <abhijeetrou123@gmail.com>
2024-05-26 00:40:25 +0530

    Initial commit

---
+++ React/State_Hooks/Vite + React/a3p1/src/components/StopWatch.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+function StopWatch() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (count < 10) {
+        setCount(prevCount => prevCount + 1);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [count]);
+
+  return (
+    <div style={{ border: "1px dashed red", padding: "10px", margin: "10px" }}>
+      <h1>Stop Watch</h1>
+      <h4>{count}</h4>
+    </div>
+  );
+}
+
+export default StopWatch;
